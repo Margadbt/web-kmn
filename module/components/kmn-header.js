054bc611b2ea8fe0b756b1bd62bd008ebf3c385c
Margadbt <bmargad17@gmail.com>
2023-11-05 18:37:49 +0800

    .

--- module/components/kmn-header.js
+++ module/components/kmn-header.js
@@ -37,6 +37,11 @@ header{
                 border-radius: 8px;
             }
         }
+
+        & .right{
+            display: flex;
+            gap: 5px;
+        }
     }
 }
 #logo{
@@ -48,24 +53,92 @@ header{
 .mobile{
     display: none;
 }
+#mbtn{
+    display: none;
+}
 
-@media screen and (max-width:762px){
-    .desktop{
+
+.mobile{
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    display: none;
+    justify-content: center;
+    align-items: center;
+    backdrop-filter: blur(8px);
+    background-color: rgb(0, 0, 0, 0.5);
+    
+    
+    & nav{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        background-color: #fff;
+        width: 200px;
+        height: 200px;
+        box-shadow: var(--shadow-card);
+        border: var(--border-card);
+        border-radius: var(--border-rad);
+        & ul{
+            display: flex;
+            align-items: right;
+            flex-direction: column;
+            justify-content: right;
+
+            & li{
+                list-style-type: none;
+                margin-bottom: 0.5em;
+
+                & a{
+                    text-decoration: none;
+                    color: black;
+                    font-weight: 600;
+                }
+            }
+
+            & li.last{
+                margin-right: 0;
+            }
+
+            & li.active{
+                border-left: 3px solid var(--color-main);
+                padding-left: 5px;
+                font-weight:800;
+            }
+        }
+    }
+}
+
+@media screen and (max-width: 1023px) {
+    ul.desktop {
         display: none;
     }
-    .mobile{
-
+    .active{
+        display: flex;
     }
-    header{
-        padding: 0 10%;
+    #mbtn{
+        display: inline-block;
+    }
+
 }
+
+@media screen and (max-width:767px){
+    header{
+        & nav{
+            padding: 0 10%;
+        }
+    }
+}
+    
 `;
+
+
+
+
 class Header extends HTMLElement {
     constructor() {
-        super();        
-    }
-    connectedCallback() {
-
+        super();
         const currentPageURL = window.location.pathname
 
         this.attachShadow({ mode: "open" });
@@ -75,25 +148,53 @@ class Header extends HTMLElement {
             ${style}
             </style>
             <header>
-                
                 <nav>
                 <img id="logo" src="/assets/logoT.svg" />
-                    <ul>
+                    <ul class="desktop">
                     <li class="${currentPageURL === '/' ? 'active' : ''}"><a href="/">Home</a></li>
                         <li class="${currentPageURL === '/pages/test.html' ? 'active' : ''}"><a href="/pages/test.html">Test</a></li>
                         <li class="${currentPageURL === '/pages/community.html' ? 'active' : ''}"><a href="/pages/community.html">Community</a></li>
                         <li class="${currentPageURL === '/pages/plan.html' ? 'active' : ''} last"><a href="/pages/plan.html">Plan</a></li>
                     </ul>
-                    <img id="pfp" src="/assets/pfp.png" />
+                    <div class="right">
+                        <img id="pfp" src="/assets/pfp.png" />
+                        <button id="mbtn">M</button>
+                    </div>
                 </nav>
-                
             </header>
+            <div class="mobile">
+                <nav>
+                <h2>Menu</h2>
+                <ul>
+                    <li class="${currentPageURL === '/' ? 'active' : ''}"><a href="/">Home</a></li>
+                    <li class="${currentPageURL === '/pages/test.html' ? 'active' : ''}"><a href="/pages/test.html">Test</a></li>
+                    <li class="${currentPageURL === '/pages/community.html' ? 'active' : ''}"><a href="/pages/community.html">Community</a></li>
+                    <li class="${currentPageURL === '/pages/plan.html' ? 'active' : ''} last"><a href="/pages/plan.html">Plan</a></li>
+                </ul>
+                </nav>
+            </div>
         `;
+    }
+    connectedCallback() {
+        const mbtn = this.shadowRoot.getElementById("mbtn");
+        const mobileDiv = this.shadowRoot.querySelector(".mobile");
+
+        mbtn.addEventListener("click", () => {
+            mobileDiv.classList.toggle("active");
+        });
+
+        mobileDiv.addEventListener("click", ()=>{
+            mobileDiv.classList.toggle("active");
+        })
+
     }
 
     adoptedCallback() {
         //implementation
+        
     }
 }
+
+
 
 customElements.define("kmn-header", Header);
