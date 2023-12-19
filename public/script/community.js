8c15c430bfab8e4ae5d7ab5f6f1ecbc22c45ea76
Margadbt <bmargad17@gmail.com>
2023-12-20 01:25:17 +0800

    connected db and did some crud things

--- public/script/community.js
+++ public/script/community.js
@@ -9,7 +9,7 @@ class Community {
       
       const gresponse = await fetch("/api/groups");
       const gData = await gresponse.json();
-      const groups = gData.groups;
+      const groups = gData;
       
       let htmlGroups = ``;
       for (const group of groups) {
@@ -27,25 +27,27 @@ class Community {
       if (id) {
         response = await fetch(`/api/posts/${id}`);
         posts = await response.json();
+
       } else {
         response = await fetch(`/api/posts`);
         data = await response.json();
-        posts = data.posts;
+        posts = data;
+        document.querySelector(".write-post").style.display = "none";
       }
       
 
       let htmlPosts = ``;
       for (const post of posts) {
-        let ggname = groups.find(g => g.id == post.groupId);
+        let ggname = groups.find(g => g.groupid == post.groupid);
         const groupName = ggname.name;
         
-        htmlPosts += `<kmn-post id="${post.id}" userId="${post.userId}" description="${post.description}" likeCount="${post.likeCount}" photoURL="${post.photoURL}" groupName="${groupName}"></kmn-post>`;
+        htmlPosts += `<kmn-post postid="${post.postid}" userId="${post.userid}" description="${post.description}" likeCount="${post.likecount}" photoURL="${post.photoURL}" commentcount="${post.commentcount}" groupName="${groupName}"></kmn-post>`;
       }
       document
         .getElementById("posts")
         .insertAdjacentHTML("beforeend", htmlPosts);
       if(id>0){
-        let gname = groups.find(g => g.id == id);
+        let gname = groups.find(g => g.groupid == id);
         document.getElementById("group-name").innerText = gname.name;
         document.querySelector(".group-info-right").innerHTML = `<button class="group-info-leave-button"><img src="/public/assets/icons/group.svg" />Группээс гарах</button>`;
       }
@@ -62,16 +64,62 @@ class Community {
 
 class Group {
   constructor(group) {
-    this.id = group.id;
+    this.groupid = group.groupid;
     this.name = group.name;
   }
 
   Render() {
     return `
-            <li><a href="?group=${this.id}">${this.name}</a></li>
+            <li><a href="?group=${this.groupid}">${this.name}</a></li>
         `;
   }
 }
 
 var com = new Community();
 com.Init();
+
+const postBtn = document.querySelector(".write-post-post-btn");
+// postBtn.addEventListener("click", createPost());
+
+function createPost() {
+  console.log("hi");
+  const postInput = document.querySelector(".write-post-input");
+  const description = postInput.value;
+
+  if (!description.trim()) {
+    alert("Post хоосон байна!");
+    return;
+  }
+
+  const postData = {
+    userid: 1,
+    groupid: id, 
+    description: description,
+    likecount: 0,
+    photo: "" ,
+    commentcount: 0,
+  };
+
+  fetch('/api/posts/create', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(postData),
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.status;
+  })
+  .then(data => {
+    console.log('Post created successfully:', data);
+    location.reload();
+    // You can also update your UI or do other actions after successful post creation
+  })
+  .catch(error => {
+    console.error('Error creating post:', error);
+    // Handle errors, show an alert, etc.
+  });
+}
