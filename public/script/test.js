41c1c3c1f28d423cdc577576223e51d4faf0ba1f
khanka0511 <khantushig50511@gmail.com>
2023-12-20 03:26:40 +0800

    [feat]: get the final MBTI results

--- public/script/test.js
+++ public/script/test.js
@@ -1,85 +1,143 @@
-// Define a function to create the radio group component
-function createRadioGroup(questionText, options, containerId) {
-  // Create the legend element
-  const legend = document.createElement("legend");
-  legend.classList.add("sr-only");
-  legend.innerHTML = `<span class="statement text-center">${questionText}</span>`;
-  fieldset.appendChild(legend);
+let currentQuestionIndex = 0;
+const questionsPerPage = 5;
+const responses = [];
 
-  // Create the group options container
-  const groupOptions = document.createElement("div");
-  groupOptions.classList.add("group__options");
+async function loadNextQuestion() {
+  try {
+    const quizContainer = document.getElementById("quiz-container");
+    const radioButtons = document.getElementsByTagName("input");
 
-  // Iterate through options and create radio inputs
-  options.forEach((option, index) => {
-    const inputId = `radio_${containerId}_${index}`;
-    const input = `<fieldset class="">
-              <!-- Render the question and options dynamically -->
-              <legend class="question">${currentQuestion.question}</legend>
-              <div class="group_options">
-                <div aria-hidden="true" class="agree">
-                  Agree
+    const apiUrl = `https://api.jsonbin.io/v3/b/6578dbe7266cfc3fde680095/latest`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    const questions = data.record.questions;
+
+    const startQuestionIndex = currentQuestionIndex;
+    const endQuestionIndex = Math.min(
+      currentQuestionIndex + questionsPerPage,
+      questions.length
+    );
+
+    function combineResponses(selectedValues) {
+      responses.push(...selectedValues);
+    }
+    window.scrollTo({ top: 0, behavior: "smooth" });
+
+    if (startQuestionIndex < endQuestionIndex) {
+      let questionHTML = "";
+      let qCount = 0; // Question name counter
+
+      for (let i = startQuestionIndex; i < endQuestionIndex; i++, qCount++) {
+        const currentQuestion = questions[i];
+        questionHTML += `
+              <fieldset class="">
+                <legend class="question">${currentQuestion.question}</legend>
+                <div class="group_options">
+                  <div aria-hidden="true" class="agree">
+                    Agree
+                  </div>
+                  <div id="radios">
+                    <label class="container">
+                      <input name="radio-${qCount}" type="radio" value="-3"/>
+                      <span class="chosen very"></span>  
+                    </label>
+                    <label class="container">
+                      <input name="radio-${qCount}" type="radio" value="-2"/>
+                      <span class="chosen more"></span>  
+                    </label>
+                    <label class="container">
+                      <input name="radio-${qCount}" type="radio" value="-1"/>
+                      <span class="chosen slight"></span>  
+                    </label>
+                    <label class="container">
+                      <input name="radio-${qCount}" type="radio" value="0"/>
+                      <span class="chosen neutral"></span>  
+                    </label>
+                    <label class="container">
+                      <input name="radio-${qCount}" type="radio" value="1"/>
+                      <span class="chosen slight"></span>  
+                    </label>
+                    <label class="container">
+                      <input name="radio-${qCount}" type="radio" value="2"/>
+                      <span class="chosen more"></span>  
+                    </label>
+                    <label class="container">
+                      <input name="radio-${qCount}" type="radio" value="3"/>
+                      <span class="chosen very"></span>  
+                    </label>
+                  </div>
+                  <div aria-hidden="false" class="disagree">
+                    Disagree
+                  </div>
                 </div>
-                <div id="radios">
-                  <label class="container">
-                    <input name="radio" type="radio" value="-3"/>
-                    <span class="chosen very"></span>  
-                  </label>
-                  <label class="container">
-                    <input name="radio" type="radio" value="-2"/>
-                    <span class="chosen more"></span>  
-                  </label>
-                  <label class="container">
-                    <input name="radio" type="radio" value="-1"/>
-                    <span class="chosen slight"></span>  
-                  </label>
-                  <label class="container">
-                    <input name="radio" type="radio" value="0"/>
-                    <span class="chosen neutral"></span>  
-                  </label>
-                  <label class="container">
-                    <input name="radio" type="radio" value="1"/>
-                    <span class="chosen slight"></span>  
-                  </label>
-                  <label class="container">
-                    <input name="radio" type="radio" value="2"/>
-                    <span class="chosen more"></span>  
-                  </label>
-                  <label class="container">
-                    <input name="radio" type="radio" value="3"/>
-                    <span class="chosen very"></span>  
-                  </label>
-                </div>
-                <div aria-hidden="false" class="disagree">
-                  Disagree
-                </div>
-              </div>
-            </fieldset>`;
-    groupOptions.innerHTML += input;
-  });
+              </fieldset>
+            `;
+      }
 
-  // Append elements to the fieldset
-  fieldset.appendChild(groupOptions);
+      const selectedValues = Array.from(radioButtons)
+        .map((radio) => ({
+          originalValue: radio.value,
+          checked: radio.checked,
+        }))
+        .filter((obj) => obj.checked)
+        .map((obj) => obj.originalValue);
 
-  // Append the fieldset to the specified container
-  const container = document.getElementById(containerId);
-  container.appendChild(fieldset);
+      combineResponses(selectedValues);
+
+      quizContainer.innerHTML = questionHTML;
+    } else {
+      alert("Quiz completed!");
+      const selectedValues = Array.from(document.getElementsByTagName("input"))
+        .map((radio) => ({
+          originalValue: radio.value,
+          checked: radio.checked,
+        }))
+        .filter((obj) => obj.checked)
+        .map((obj) => obj.originalValue);
+
+      combineResponses(selectedValues);
+      console.log(responses);
+
+      // Assign results by chunk summary
+      const chunkSize = 12;
+
+      var resultsBySummary = [],
+        j = 0;
+
+      for (let i = 0; i < responses.length; i += chunkSize, j++) {
+        const chunk = responses.slice(i, i + chunkSize);
+
+        const summary = chunk.reduce(
+          (sum, value) => sum + parseInt(value, 10),
+          0
+        );
+
+        console.log(`Letter chunk ${i / chunkSize + 1}: ${summary}`);
+        resultsBySummary[j] = summary;
+      }
+
+      var finalResult = "";
+      // Assign the letters for results
+      resultsBySummary[0] > 0 ? (finalResult += "E") : (finalResult += "I");
+      resultsBySummary[1] > 0 ? (finalResult += "S") : (finalResult += "N");
+      resultsBySummary[2] > 0 ? (finalResult += "T") : (finalResult += "F");
+      resultsBySummary[3] > 0 ? (finalResult += "J") : (finalResult += "P");
+      finalResult += "-";
+      resultsBySummary[4] < 0 ? (finalResult += "T") : (finalResult += "A");
+
+      console.log(finalResult);
+    }
+
+    currentQuestionIndex = endQuestionIndex;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+
+  function getOverallResponses() {
+    return responses;
+  }
 }
 
-// Example usage
-const questionText = "You regularly make new friends.";
-const options = [
-  { label: "I strongly agree", value: "-3" },
-  { label: "I moderately agree", value: "-2" },
-  { label: "I agree", value: "-1" },
-  { label: "I am not sure", value: "0" },
-  { label: "I disagree", value: "1" },
-  { label: "I moderately disagree", value: "2" },
-  { label: "I strongly disagree", value: "3" },
-];
-
-// Specify the container ID where the component should be added
-const containerId = "start-quiz";
-
-// Create the radio group component
-createRadioGroup(questionText, options, containerId);
+loadNextQuestion();
