01d6a2ef36bc8e06f058aca97551539976c60357
khanka0511 <khantushig50511@gmail.com>
2024-01-01 01:53:06 +0800

    [add]: registration logic and encryption of passwd

--- public/script/register.js
+++ public/script/register.js
@@ -1,24 +1,26 @@
-async function login(event) {
+async function register(event) {
   event.preventDefault();
 
+  const username = document.getElementById("username").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   try {
-    const response = await fetch("http://localhost:4000/login", {
+    const response = await fetch("http://localhost:4000/register", {
       method: "POST",
       cache: "no-cache",
       headers: {
         "Content-Type": "application/json; charset=UTF-8",
       },
       body: JSON.stringify({
+        username: username,
         email: email,
         password: password,
       }),
     });
 
     if (!response.ok) {
-      const errorHTML = `<p style="color: red; text-align: center;">Нэвтрэхэд алдаа гарлаа</p>`;
+      const errorHTML = `<p style="color: red; text-align: center;">Registration failed</p>`;
       document
         .querySelector(".card")
         .insertAdjacentHTML("afterbegin", errorHTML);
@@ -26,16 +28,9 @@ async function login(event) {
       console.log(response);
       return;
     }
-    window.location.href = "/";
+    console.log("i mean it at least loading");
+    window.location.href = "/login";
   } catch (error) {
-    console.error("Error during login:", error);
-  }
-}
-
-async function logout() {
-  try {
-    const response = await fetch("http://localhost:3000/user/logout");
-  } catch (error) {
-    console.error("Error during logout:", error);
+    console.error("Error during registration:", error);
   }
 }
