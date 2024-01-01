36a7277bf7774080c15354cab0661cb0cbe4f0a7
khanka0511 <khantushig50511@gmail.com>
2024-01-01 16:00:25 +0800

    [dunno]: registration test

--- login.js
+++ login.js
@@ -1,41 +1,54 @@
 class Login {
-    constructor() {
-        this.sessions = new Map();
-        this.users = new Map();
-    }
-    
-    verifyLogin(req, res) {
-        const email = req.body.email;
-        const pass = req.body.password;
+  constructor() {
+    this.sessions = new Map();
+    this.users = new Map();
+  }
 
+  verifyLogin(req, res) {
+    const email = req.body.email;
+    const pass = req.body.password;
 
-        if (!this.users.has(email) || this.users.get(email).password !== pass) {
-            res.status(403).end();
-            return;
-        }
-
-        const sid = Math.floor(Math.random() * 100_000_000_000_000);
-        this.sessions.set(sid, {
-            user: email,
-            user_id: this.users.get(email).user_id,
-            fullname: this.users.get(email).fullname,
-            logged: Date.now()
-        });
-        console.log(this.sessions);
-
-        res.status(200);
-        res.cookie("session_id", sid);
-        res.send({
-            result: "OK",
-            username: this.users.get(email).fullname
-        });
-        res.redirect('/');
+    if (!this.users.has(email) || this.users.get(email).password !== pass) {
+      res.status(403).end();
+      return;
     }
 
-    logout(sessionId) {
-        this.sessions.delete(sessionId);
-        console.log(`Session ${sessionId} logged out`);
-    }
+    const sid = Math.floor(Math.random() * 100_000_000_000_000);
+    this.sessions.set(sid, {
+      user: email,
+      user_id: this.users.get(email).user_id,
+      fullname: this.users.get(email).fullname,
+      logged: Date.now(),
+    });
+    console.log(this.sessions);
+
+    res.status(200);
+    res.cookie("session_id", sid);
+    res.send({
+      result: "OK",
+      username: this.users.get(email).fullname,
+    });
+    res.redirect("/");
+  }
+
+  logout(sessionId) {
+    this.sessions.delete(sessionId);
+    console.log(`Session ${sessionId} logged out`);
+  }
+
+  registerUser(username, email, password) {
+    const user_id = Math.floor(Math.random() * 100_000);
+
+    this.users.set(email, {
+      user_id: user_id,
+      password: password,
+      fullname: fullname,
+    });
+
+    console.log(this.users.values);
+
+    console.log(`User ${email} registered`);
+  }
 }
 
 const login = new Login();
