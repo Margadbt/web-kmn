35bd5b00929eb27e3060dd8a3334dd98f139d615
Margadbt <bmargad17@gmail.com>
2024-01-03 00:16:08 +0800

    .

--- routes/api/mbtiRoutes.js
+++ routes/api/mbtiRoutes.js
@@ -8,18 +8,15 @@ const pool = new Pool({
     "postgres://default:pKvlL1OiN8Ac@ep-nameless-glade-19285305-pooler.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
 });
 
-router.post("/result", (req, res) => {
-  pool.query(
-    `INSERT INTO mbtiresult (result) VALUES ('${req.body.result}')`,
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Internal Server Error");
-        return;
-      }
-      res.status(201).json(result.rows[0]);
+router.post("/result/change", (req, res) => {
+  pool.query(`UPDATE "user" SET mbti_result='${req.body.mbti_result} WHERE user_id=${req.body.user_id}'`, (err, result)=>{
+    if (err) {
+      console.log(err)
+      res.status(500).send('Internal Server Error');
+      return;
     }
-  );
+    res.status(201).json(result.rows[0]);
+  })
 });
 
 module.exports = router;
